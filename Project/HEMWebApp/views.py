b8b45147a73384c9525e879ea630172d4caa8677
JimPaan <jimpaan9070@gmail.com>
2024-07-20 18:50:55 +0800

    Account Management Update

--- Project/HEMWebApp/views.py
+++ Project/HEMWebApp/views.py
@@ -1,7 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'mainpage.html')
+    return render(request, 'index.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid username or password. Please try again.')
+            return redirect('login')
+
+    return render(request, 'login.html')
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('cpassword')
+
+        # Check if passwords match
+        if password != confirm_password:
+            # Handle password mismatch error
+            messages.error(request, 'Password do not match!')
+            return redirect('signup')
+
+        # Create user
+        User.objects.create_user(
+            email=email,
+            password=password,
+            username=username,
+        )
+        return redirect('login')
+
+    return render(request, 'signup.html')
