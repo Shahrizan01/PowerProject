22d6ab899a7a4f1a54d2bf57c230b5d01e25ced3
AliffIzham <aliffizham1660@gmail.com>
2024-07-21 17:58:02 +0800

    Main Page

--- Project/HEMWebApp/urls.py
+++ Project/HEMWebApp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login_view, name='login'),
+    path('signup', views.signup_view, name='signup'),
+    path('homepage', views.homepage_view, name= 'homepage'),
+    path('logout', views.logout_view, name= 'logout'),
 ]
--- Project/HEMWebApp/views.py
+++ Project/HEMWebApp/views.py
@@ -1,7 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
 def index(request):
     return render(request, 'mainpage.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('homepage')
+        else:
+            messages.error(request, 'Invalid username or password. Please try again.')
+            return redirect('login')
+
+    return render(request, 'login.html')
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('cpassword')
+
+        # Check if passwords match
+        if password != confirm_password:
+            # Handle password mismatch error
+            messages.error(request, 'Password do not match!')
+            return redirect('signup')
+
+        # Create user
+        User.objects.create_user(
+            email=email,
+            password=password,
+            username=username,
+        )
+        return redirect('login')
+
+    return render(request, 'signup.html')
+
+def homepage_view(request):
+    if request.user.is_authenticated == True:
+        return render(request, 'homepage.html')
+    else:
+        return redirect('login')
+
+
+def logout_view(request):
+    if request.user.is_authenticated == True:
+        username = request.user.username
+    else:
+        username = None
+    
+    if username != None:
+        logout(request)
+        return redirect(index)
